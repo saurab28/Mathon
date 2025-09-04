4d7388ee640dc7f35432e849addfa0fb1bde0a76
saurab28 <sarafsaurab24@gmail.com>
2025-09-04 16:05:11 +0530

    remove-img-tag

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,5 @@ import { RouterView } from 'vue-router';
 </template>
 
 <style scoped>
-.img{
 
-  margin: 0;
-  /* height: 100vh; */
-  /* background-color: #f5f0e6; beige-like background ; */
-  /* background-image: 
-    linear-gradient(to right, rgba(0, 0, 0, 0.05) 1px, transparent 1px),
-    linear-gradient(to bottom, rgba(0, 0, 0, 0.05) 1px, transparent 1px); */
-  /* background-size: 40px 40px; grid square size */ 
-}
 </style>
