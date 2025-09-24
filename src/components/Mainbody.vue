7616d8412711bed279bf7683ffe4f08ace35d285
saurab28 <sarafsaurab24@gmail.com>
2025-09-24 11:56:25 +0530

    changes-style

--- src/components/Mainbody.vue
+++ src/components/Mainbody.vue
@@ -37,17 +37,19 @@ const compName = computed(()=>{
 
 <template>
     <div class="flex flex-col items-center">
-        <p class="text-4xl sm:text-5xl md:text-6xl lg:text-[80px] font-bold font-libertinus text-center px-4">Mathon...</p>
-        <div class="h-12 sm:h-16 md:h-[70px] bg-amber-400 w-auto mt-2 sm:mt-[10px] mx-4">
+        <p class="text-3xl sm:text-5xl md:text-6xl lg:text-[80px] font-bold font-libertinus text-center px-4">Mathon...</p>
+        <div class="mt-5">
+          <div class="h-12 sm:h-16 md:h-[70px] bg-amber-400 w-auto  sm:mt-[10px] mx-4">
             <div class="bg-white h-12 sm:h-16 md:h-[70px] flex items-center relative bottom-2 right-4 sm:bottom-[6px] sm:right-[12px] md:bottom-[10px] md:right-[20px] px-2 sm:px-4">
                 <p class="text-sm sm:text-lg md:text-xl lg:text-[30px] font-roboto text-center w-full">"Smart Calculators for Everyday Life"</p>
             </div>
+          </div>
         </div>
         <p class="text-base sm:text-lg md:text-[20px] mt-12 sm:mt-16 md:mt-[100px] font-playfair backdrop-blur-3xl opacity-1.3 text-center px-4 max-w-4xl">From solving math problems to managing finances and tracking health, MathOn gives you fast, accurate, and easy-to-use calculators in one place</p>
         <p class="text-xl sm:text-2xl md:text-[26px] font-cormorant font-semibold backdrop-blur-3xl opacity-1.3 mt-8 sm:mt-12 md:mt-[60px] text-center px-4">Explore all our math tools ...</p>
-        <div class="flex flex-col sm:flex-row w-full px-4 sm:px-8 md:px-16 lg:px-[344px] justify-center sm:justify-evenly mt-8 sm:mt-12 md:mt-[60px] items-center gap-8 sm:gap-4">
+        <div class="flex flex-col sm:flex-row w-full px-4 sm:px-8 md:px-16   justify-center sm:justify-evenly mt-8 sm:mt-12 md:mt-[60px] items-center gap-8 sm:gap-4">
             <div class="text-center text-base sm:text-lg md:text-[18px] font-playfair space-y-4 hover:font-medium cursor-pointer" @click="changeCom('A')">
-                <img :src="Airthematic" class="h-48 w-44 sm:h-56 sm:w-48 md:h-[250px] md:w-[220px] bg-cover rounded-[12px] hover:scale-105 transform transition duration-300 ease-in-out mx-auto"/>
+                <img :src="Airthematic" class="h-48 w-44 sm:h-56 sm:w-48 md:h-[250px] md:w-[220px]  bg-cover rounded-[12px] hover:scale-105 transform transition duration-300 ease-in-out mx-auto"/>
                 <p>Arithmetic</p>
             </div>
             <div class="text-center text-base sm:text-lg md:text-[18px] font-playfair hover:font-medium space-y-4 cursor-pointer" @click="changeCom('B')">
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,26 +17,26 @@ const handleAuth = () => {
 </script>
 
 <template>
-    <nav class="flex items-center backdrop-blur-3xl opacity-1.3 pt-4 pb-4 md:pt-[20px] md:pb-[20px] justify-between w-full px-4 sm:px-8 md:px-16 lg:px-[104px]">
-        <div class="flex items-center">
+    <nav class="flex items-center backdrop-blur-3xl opacity-1.3 pt-4 pb-4 md:pt-[20px] md:pb-[20px] justify-between w-full px-4 sm:px-8 md:px-[30px] lg:px-[50px]">
+        <div class="flex items-center ">
             <img :src="Mathonlogo" class="h-16 w-12 sm:h-20 sm:w-16 md:h-[100px] md:w-[80px]"/>
             <div class="ml-2 sm:ml-3 md:ml-[14px] flex items-center">
                 <div class="mr-[8px]" v-for="value in source">
-                    <div class="size-6 sm:size-8 md:size-[40px] p-1 md:p-[4px] bg-amber-400 mr-1 sm:mr-2 md:mr-[8px]">
-                        <div class="size-6 sm:size-8 md:size-[40px] p-1 md:p-[4px] relative bottom-2 right-4 sm:bottom-[6px] sm:right-[12px] md:bottom-[10px] md:right-[20px] bg-white ml-1 sm:ml-1.5 md:ml-2">
-                            <span class="text-xs sm:text-sm md:text-[20px] font-mono font-bold">{{ value }}</span>
+                    <div class="size-4 sm:size-8 md:size-[40px] p-1 md:p-[4px] bg-amber-400 mr-1 sm:mr-2 md:mr-[8px] ">
+                        <div class="size-4 sm:size-8 md:size-[40px]  md:p-[4px] bottom-2 right-3 sm:bottom-[10px] relative sm:right-[16px] md:bottom-[10px] md:right-[20px] bg-white ml-1 sm:ml-1.5 md:ml-2">
+                            <span class="text-[10px] sm:text-lg md:text-[20px] font-mono font-bold absolute top-1 left-1">{{ value }}</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="h-8 sm:h-10 md:h-[40px] bg-amber-400">
-            <div class="h-8 sm:h-10 md:h-[40px] px-3 py-1 sm:px-4 sm:py-2 md:p-[14px] relative bottom-2 right-2 sm:bottom-[6px] sm:right-[6px] md:bottom-[10px] md:right-[10px] text-center bg-white flex items-center text-sm sm:text-base md:text-[20px] font-medium" @click="handleAuth">
+        <div class="h-6 sm:h-10 md:h-[40px] bg-amber-400">
+            <div class="h-6 sm:h-10 md:h-[40px] px-3 py-1 sm:px-4 sm:py-2 md:p-[14px] relative bottom-2 right-2 sm:bottom-[10px] sm:right-[10px] md:bottom-[10px] md:right-[10px] text-center bg-white flex items-center text-sm sm:text-base md:text-[20px] font-medium" @click="handleAuth">
                 {{ userAuthSta }}
             </div>
         </div>
     </nav>
-    
+
 
 
 </template>
